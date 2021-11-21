805ab22e458dcbee84f82e6c644d3f00c3f2c243
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-11-21 11:39:17 -0300

    add auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { VitePWA } from 'vite-plugin-pwa'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-	plugins: [Vue(), WindiCSS(), VitePWA()],
+	plugins: [
+		Vue(),
+		WindiCSS(),
+		VitePWA(),
+		AutoImport({
+			include: [
+				/\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+				/\.vue$/,
+				/\.vue\?vue/, // .vue
+				/\.md$/, // .md
+			],
+			imports: ['vue'],
+		}),
+	],
 })
